f8a875e61f50fcf51dcfc744bc2891f115072bb1
antgalesp <62609761+antgalesp@users.noreply.github.com>
2020-06-01 11:30:40 +0200

    Eliminacion de bucles for para mas rapidez

--- seam_cost_standard.m
+++ seam_cost_standard.m
@@ -1,22 +1,22 @@
 function vertex_cost = seam_cost_standard(img, mask_delete, mask_protect)
-% Calcular los costos de los vÈrtices para la tarea de tallar las costuras.
-% Los costos de los vÈrtices para los pÌxeles individuales se basan en el 
+% Calcular los costos de los v√©rtices para la tarea de tallar las costuras.
+% Los costos de los v√©rtices para los p√≠xeles individuales se basan en el 
 % gradiente estimado de la imagen.
 %
 % Entrada:
-%    img [MxNx3 (doble)] entrada RGB mask_delete [MxN (lÛgico)] pixeles de 
-%    especulaciÛn de matriz para los cuales el costo del vÈrtice debe ser lo 
+%    img [MxNx3 (doble)] entrada RGB mask_delete [MxN (l√≥gico)] pixeles de 
+%    especulaci√≥n de matriz para los cuales el costo del v√©rtice debe ser lo 
 %    suficientemente bajo para asegurar su prioridad de tallado mask_protector 
-%    [MxN (lÛgico)] pixeles de especulaciÛn de matriz para los cuales el 
-%    costo del vÈrtice debe ser lo suficientemente bajo para asegurar su 
+%    [MxN (l√≥gico)] pixeles de especulaci√≥n de matriz para los cuales el 
+%    costo del v√©rtice debe ser lo suficientemente bajo para asegurar su 
 %    prioridad de tallado
 %
 % Salida:
-%    costo_vÈrtice [MxN (doble)] costo del vÈrtice para los pÌxeles 
-%    individuales basado en el gradiente estimado de la imagen y las m·scaras
-%    binarias que especifican los pÌxeles a ser eliminados y protegidos
+%    costo_v√©rtice [MxN (doble)] costo del v√©rtice para los p√≠xeles 
+%    individuales basado en el gradiente estimado de la imagen y las m√°scaras
+%    binarias que especifican los p√≠xeles a ser eliminados y protegidos
 
-% estimar los derivados parciales y calcular el coste_vÈrtice
+% estimar los derivados parciales y calcular el coste_v√©rtice
 
 
 
@@ -32,25 +32,22 @@ vertex_cost(:,1)=[];
 vertex_cost(:,N)=[];
 
 if exist('mask_delete', 'var')
-    %modificar el coste_vÈrtice de los pÌxeles que deben ser eliminados
-    for i=1:M
-        for j=1:N
-            if(mask_delete(i,j)==1)
-                vertex_cost(i,j)=-2*M;
-            end
-        end
-    end
+%modificar el coste_v√©rtice de los p√≠xeles que deben ser eliminados
+    newValues = (-2 * M) * mask_delete;
+    opositeMask = mask_delete < 1;
+    vertexWithoutMask = opositeMask .* vertex_cost;
+    vertex_cost = vertexWithoutMask + newValues;
+    
+    
 end
 
 if exist('mask_protect', 'var')
-    % modificar el coste_vÈrtice de los pÌxeles protegidos
-    for i=1:M
-        for j=1:N
-            if(mask_protect(i,j)==1)
-                vertex_cost(i,j)=2*M;
-            end
-        end
-    end
+% modificar el coste_v√©rtice de los p√≠xeles protegidos
+    newValues = (2 * M) * mask_protect;
+    opositeMask = mask_protect < 1;
+    vertexWithoutMask = opositeMask .* vertex_cost;
+    vertex_cost = vertexWithoutMask + newValues;
+    
 end
 
 end
